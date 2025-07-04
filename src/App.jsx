828aa5ef9cbce6b25f3f46be1eea9d1c1d36e1b5
hoyennhi489 <hyn489@gmail.com>
2025-07-04 19:07:15 +0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>English ↔ Vietnamese Translator</h1>
+      <p>Translator component will be added later.</p>
+    </div>
+  );
+};
+
+export default App;
